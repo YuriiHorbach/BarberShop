6ef66c3e172ac0963660a346212557dd04b5d815
gyrii84@gmail.com <gyrii84@gmail.com>
2019-08-27 22:05:49 +0300

    init app

---
+++ app.rb
@@ -0,0 +1,106 @@
+#encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'pony'
+
+
+get '/' do
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
+
+get '/about' do
+	@error = 'Something wrong!'
+	erb :about
+end
+
+post '/visit' do
+	@name = params[:name]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@barber = params[:barber]
+	@color = params[:color]
+
+	# хеш
+	hh = {  :name => 'Enter name', 
+		    :phone => 'Enter phone', 
+		    :datetime => 'Enter date and time'
+		  }
+
+	# #для кожної пари ключ-значення
+	# hh.each do |key, value|		
+	# # якщо параметр порожній
+	# 	if params[key] == ''			
+	# 	# змінній @error присвоюємо value з хеша hh 
+	# 	# (це значення - повідомлення про помилку)
+	# 	# тобто змінній @error присвоїти повідомлення про помилку
+	# 		@error = hh[key]	
+	# 	# повернути view visit		
+	# 		return erb :visit
+	# 	end
+	# end
+
+	@error = hh.select{|key,_| params[key] ==""}.values.join(", ")
+
+	if @error != ""
+		return erb :visit
+	end
+
+
+	f = File.open "./public/user.txt", "a"
+	f.write "User: #{@name} Phone: #{@phone} Data and time: #{@datetime} Your barber will be: #{@barber} Your color: #{@color}"
+
+	erb :visit
+
+
+end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/contacts' do
+	@email = params[:email]
+	@message = params[:message]
+
+	hh_mail = { 
+			:email => 'Enter email' ,
+	    	:message =>'Enter message'
+		}
+
+
+	@error = hh_mail.select{|key,_| params[key] ==""}.values.join(", ")
+
+	if @error != ""
+		return erb :contacts
+	end
+
+	# @message = "Dear #{@name}, we are glad that You are with us"
+
+	f = File.open "./public/contacts.txt", "a"
+	f.write "Mail: #{@email} Message: #{@message} "
+
+# 	Pony.mail({
+#   	:to => 'gyrii84@gmail.com',
+#   	:via => :smtp,
+#   	:via_options => {
+#     :address              => 'smtp.gmail.com',
+#     :port                 => '587',
+#     :enable_starttls_auto => true,
+#     :user_name            => 'gyrii84@gmail.com',
+#     :password             => 'tapunina_2010_B18',
+#     :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+#     :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
+#   }
+# })
+# 	redirect '/success' 
+
+	erb :contacts
+
+
+end
+
+get '/contacts' do
+	erb :contacts
+end
+
