10673e796c4de65eb903c3631670afbd090886fd
southl <gyrii84@gmail.com>
2019-08-29 16:48:17 +0300

    init barbers table on configure

--- app.rb
+++ app.rb
@@ -3,6 +3,20 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
 
+def is_barber_exists? db, name
+		@db.execute('select * from Barbers where name=?',[name]).length > 0 
+
+end 
+	
+def seed_db db, barbers
+		barbers.each do |barber|
+			if !is_barber_exists? @db, barber
+				@db.execute 'insert into Barbers (name) values (?)', [barber]
+			end
+		end
+end
+
+
 def get_db
 	@db = SQLite3::Database.new 'barbershop.db'
 	@db.results_as_hash = true
@@ -24,18 +38,15 @@ configure do
 			"color" TEXT
 		)'
 
-	# @db_barbers.execute ' CREATE TABLE IF NOT EXISTS
-	# 	"Barbers"
-	# 	(
-	# 		"id" INTEGER PRIMARY KEY AUTOINCREMENT,
-	# 		"barbername" TEXT,
-	# 	)'
+	@db_barbers.execute ' CREATE TABLE IF NOT EXISTS
+		"Barbers"
+		(
+			"id" INTEGER PRIMARY KEY AUTOINCREMENT,
+			"name" TEXT
+		)'
+
+		seed_db @db, ['Jessie Pinkman', 'Walter White', 'Gus Fring', 'Mike Ehrmantraut']
 	
-
-	
-
-
-
 end
 
 get '/' do
@@ -92,9 +103,9 @@ get '/showusers' do
 
 
 
-	db = get_db
+	@db = get_db
 
-	@users = db.execute 'select * from Users' #assign result db quety to variable
+	@users = @db.execute 'select * from Users' #assign result db quety to variable
 
 	
 	 erb :showusers
